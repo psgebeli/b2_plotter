b43c54a8c6edb8d34b5f245a7289cb33c3f5d40a
Paul  Stanley Gebeline <psgebeli@go.olemiss.edu>
2023-09-27 16:14:36 -0500

    implement test fom

--- tests/test_noninteractive.py
+++ tests/test_noninteractive.py
@@ -12,11 +12,15 @@ df_ccbar = rp.read_root(ccbar, key='xic_tree', columns = mycols)
 plotter = Plotter(isSigvar='xic_isSignal', mcdfs={'ccbar': df_ccbar}, signaldf = df_ccbar, interactive = False)
 
 def test_constructor():
-
     assert isinstance(plotter, Plotter)
 
 def test_plot():
-    
     for var in mycols[:-3]:
         plotter.plot(var, cuts = xicmassrangeloose)
         assert os.path.isfile(f'plot_{var}.png')
+
+def test_plotFom():
+    for var in mycols[:-3]:
+        optimal, fommax = plotter.plotFom(var, massvar = 'xic_M', signalregion = (2.46, 2.475))
+        assert os.path.isfile(f'fom_{var}.png')
+        assert isinstance(optimal, float) and isinstance(fommax, float)
